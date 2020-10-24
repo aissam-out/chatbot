9928daab44a84c17614f92beab285fabf3260376
aissam-out <39061689+aissam-out@users.noreply.github.com>
2020-10-24 09:35:34 +0100

    create new event -with comments-

--- appointments/create_event.py
+++ appointments/create_event.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 from cal_setup import get_calendar_service
 
-def create_event(start, end, summary='pas de nom', description='pas de description'):
-    # creates one hour event tomorrow 10 AM IST
+def create_event(start, end, summary='no summary', description='no description'):
+    # authentication
     service = get_calendar_service()
-
+    # add event
     event_result = service.events().insert(calendarId='primary',
         body={
             "summary": summary,
@@ -13,7 +13,7 @@ def create_event(start, end, summary='pas de nom', description='pas de descripti
             "end": {"dateTime": end, "timeZone": 'Etc/GMT+1'},
         }
     ).execute()
-
+    # print the event's fields
     print("created event")
     print("id: ", event_result['id'])
     print("summary: ", event_result['summary'])
@@ -23,4 +23,5 @@ def create_event(start, end, summary='pas de nom', description='pas de descripti
     return event_result['id']
 
 if __name__ == '__main__':
+    # for test
     create_event('2020-10-13T14:30:00+01:00', 1, 'not default summary', 'not default description')
