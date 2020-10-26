3a97efffd99f271050d573847cdb36cf3360521a
aissam-out <39061689+aissam-out@users.noreply.github.com>
2020-10-26 09:30:52 +0100

    handle time format -with comments-

--- appointments/handle_time.py
+++ appointments/handle_time.py
@@ -1,13 +1,20 @@
 from datetime import datetime, timedelta
 
 def process_time(param_time, param_date, duration=1, z=True):
-    '''convert rfc_3339 to useable time format'''
+    '''
+    convert rfc_3339 to useable time format
+    input format : '2020-10-13T14:30:00+01:00'
+    '''
 
+    # isolate date and time
     date = param_date.split('T')[0]
     time = param_time.split('+')[0].split('T')[1]
-    dt = date + 'T' + time
 
+    # convert date & time to 'dateTtime' object
+    dt = date + 'T' + time
     datetime_object = datetime.fromisoformat(dt)
+
+    # create end time & add Z to datetime object if needed
     if z:
         start = datetime_object.isoformat() + 'Z'
         end = (datetime_object + timedelta(hours=duration)).isoformat() + 'Z'
