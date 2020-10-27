7cd19dfdcfde7c66317caf7e4e094aaf3c1f8255
aissam-out <39061689+aissam-out@users.noreply.github.com>
2020-10-27 09:52:09 +0100

    check time availability before booking an appointment -with comments-

--- appointments/check_availability.py
+++ appointments/check_availability.py
@@ -1,29 +1,26 @@
-from datetime import datetime, timedelta
 from cal_setup import get_calendar_service
-#from handle_time import process_time
 
 def check(start, end):
+    '''check is a given datetime is available is not already taken'''
 
+    # authentication
     service = get_calendar_service()
 
-    print("new start : ", start)
-    print("new end : ", end)
-
-    print('Getting List o 10 events 2.0')
+    # check for availability
+    print('Getting events ...')
     events_result = service.events().list(calendarId='primary', timeMin=start, timeMax=end).execute()
     events = events_result.get('items', [])
 
     if not events:
-        print('No upcoming events found.')
+        print('It is free')
         answer = True
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
         answer = False
 
-    print(answer)
     return answer
 
 if __name__ == '__main__':
+    # for test
     check('2020-10-11T14:30:00+01:00')
-    #check('2020-10-11 09:30:00')
